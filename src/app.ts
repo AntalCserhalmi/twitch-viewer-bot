307c6c8dd8b773746e0e9eaf49a49036bb04bdfb
DEMONHUNTERWL <cserhalmiantalprog@gmail.com>
2022-06-17 23:00:52 +0200

    first-init

---
+++ src/app.ts
@@ -0,0 +1,106 @@
+import { Config, accounts } from "./config/config";
+import axios from "axios";
+import tmi from "tmi.js";
+
+const clients: Array<tmi.Client> = [];
+let expire = 0;
+let currentToken = "";
+
+const login = async (): Promise<string> => {
+    const current = Date.now() / 1000;
+
+    if (expire - current <= 0){
+        const response = await axios.post(Config.auth.url, new URLSearchParams({
+            client_id: Config.auth.clientId,
+            client_secret: Config.auth.secret,
+            grant_type: Config.auth.grant_type
+        }));
+    
+        expire = current + response.data.expires_in;
+        currentToken = response.data.access_token;
+
+        return response.data.access_token;
+    }
+
+    return currentToken;
+};
+
+
+const checkRoshIsLive = async (): Promise<boolean> => {
+    const access_token = await login();
+
+    const response = await axios.get(Config.rosh.url, {
+        params: {
+            user_id: Config.rosh.userId
+        },
+        headers: {
+            "Authorization": `Bearer ${access_token}`,
+            "Client-Id": Config.auth.clientId,
+            "Content-Type": "application/json;charset=UTF-8"
+        }
+    });
+
+    return response.data.data[0].type === "live";
+};
+
+const initAndConnectAccounts = (): void => {
+    console.log("[\x1b[36mDETECTED\x1b[0m]: Rosh started streaming...");
+    console.log("[\x1b[36mACTION\x1b[0m]: Connecting accounts...");
+    for (let i =0; i < accounts.length; i++){
+        try{
+            const client = new tmi.Client({
+                options: {
+                    debug: false
+                },
+                identity: {
+                    username: accounts[i].username,
+                    password: `oauth:${accounts[i].token}`,
+                },
+                channels: accounts[i].channels
+            });
+
+            client.connect();
+
+            client.on("connected", (a: string, b: number) => {
+                console.log(`${accounts[i].username}: ready`, a, b);
+
+                clients.push(client);
+            })
+        }catch(err){
+            console.log(err);
+        }
+    }
+};
+
+const destroyAccounts = (): void => {
+    console.log("[\x1b[36mDETECTED\x1b[0m]: Rosh went offline.")
+    console.log("[\x1b[36mACTION\x1b[0m]: Start disconnecting accounts...");
+    for (let i=0; i < clients.length; i++){
+        clients[i].disconnect();
+
+        clients.splice(i, 1);
+    }
+
+    console.log("Accounts have been disconnected");
+};
+
+const startLoop = (): void => {
+    console.log("[\x1b[36mACTION\x1b[0m]: Starting loop...");
+    setInterval(async () => {
+        const live = await checkRoshIsLive();
+
+        if (live){
+            if (clients.length === 0){
+                initAndConnectAccounts();
+            }
+        }else{
+            if (clients.length !== 0){
+                destroyAccounts();
+            }
+        }
+    }, Config.loop.time);
+};
+
+
+startLoop();
+
---
+++ src/config/config.ts
@@ -0,0 +1,93 @@
+interface IAccount{
+    username: string;
+    token: string;
+    channels: Array<string>;
+} 
+
+export const Config = {
+
+    rosh: {
+        url: "https://api.twitch.tv/helix/streams",
+        userId: 72550899
+    },
+    auth: {
+        url: "https://id.twitch.tv/oauth2/token",
+        secret: "d2rsxz483xlq5525hsfowy7lyh8mcf",
+        clientId: "thmz9p6qo7mzwdnbfsm28spmbl8v95",
+        grant_type: "client_credentials",
+    },
+
+    loop: {
+        time: 5 * 60 * 1000
+    }
+}
+
+export const accounts: Array<IAccount> = [
+    {
+        username: "typehunter11",
+        token: "qr81cgunfgcftcki5hk4qofgwc49to",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "iamjustatoxicguy",
+        token: "pulqkeqresuuj5b6bbt9mogivwaclp",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "solidhunter11",
+        token: "0vutz1qz3c6bzuaxhc1j9vkbtllvpo",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "TheBearFearerer",
+        token: "ux9yb8oounfbt7tzumv43awedvkbap",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "topicdiscover",
+        token: "ft91l0kaj4iy3jhlxj606ampdg9h0s",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "hyposwasp",
+        token: "g7bdu51319a8hakd46mdinbuiit6q2",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "smokepurp00",
+        token: "alwdmo4ua2gmrbnnak5kfnca8mzo26",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "lospista",
+        token: "b7f643duqvjz31iw8dhxsw6oy977mb",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+
+    },
+    {
+        username: "VRGMRC",
+        token: "sz2cr8izoyab3gx4b4h5o1t2xegn6q",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+
+    },
+    {
+        username: "TeTrisKkK",
+        token: "91qnnp0m7ii34rgm71tnxgyf5zpxmw",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "req_ries",
+        token: "g6jdq0qbuj5lmp6z0fuy25jno1pvh1",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "petichef",
+        token: "m7x05sa58mg0u7qhcjfvj5iyw7t921",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    },
+    {
+        username: "bw3n",
+        token: "s8am92z1aj98v4b6vffw3ehyutb7e9",
+        channels: ["roshtein", "deuceace", "vondice", "trainwreckstv", "frankdimes", "classybeef"]
+    }
+];
