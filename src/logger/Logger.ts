433b9c02a87409b224a022a6d52a342abfbb0bca
DEMONHUNTERWL <cserhalmiantalprog@gmail.com>
2022-06-19 12:24:07 +0200

    logger-updated

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -25,7 +25,7 @@ export class Logger{
 
     public getCurrentDateFormat(){
         let date = new Date();
-        return {color: this.dateColor, date:`${date.getFullYear()}-${date.getMonth() < 10? "0" + (date.getMonth()+1): (date.getMonth()+1)}-${date.getDate() < 10? "0" + date.getDate():date.getDate()} ${date.getHours() < 10?"0" + date.getHours():date.getHours()}:${date.getMinutes() < 10? "0" + date.getMinutes():date.getMinutes()}:${date.getSeconds() < 10? "0" + date.getSeconds(): date.getSeconds()}.${date.getMilliseconds()}`}
+        return {color: this.dateColor, date:`${date.getFullYear()}-${date.getMonth() < 10? "0" + (date.getMonth()+1): (date.getMonth()+1)}-${date.getDate() < 10? "0" + date.getDate():date.getDate()} ${date.getHours() + 2 < 10?"0" + date.getHours() + 2:date.getHours() + 2}:${date.getMinutes() < 10? "0" + date.getMinutes():date.getMinutes()}:${date.getSeconds() < 10? "0" + date.getSeconds(): date.getSeconds()}.${date.getMilliseconds()}`}
     }
 
     public getCallerFile(position = 2){
