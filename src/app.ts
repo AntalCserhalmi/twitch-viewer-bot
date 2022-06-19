3eb3f7b28834972ceb116013734400afddfa86c2
DEMONHUNTERWL <cserhalmiantalprog@gmail.com>
2022-06-19 12:20:37 +0200

    added-logger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { Config, accounts } from "./config/config";
 import axios from "axios";
 import tmi from "tmi.js";
+import { Logger } from "./logger/Logger";
 
+const logger = new Logger();
 const clients: Array<tmi.Client> = [];
 let expire = 0;
 let currentToken = "";
@@ -27,7 +29,7 @@ const login = async (): Promise<string> => {
 
 
 const checkRoshIsLive = async (): Promise<boolean> => {
-    console.log("[\x1b[36mACTION\x1b[0m]: Checking stream status...");
+    logger.info("Checking stream status...");
     const access_token = await login();
 
     const response = await axios.get(Config.rosh.url, {
@@ -47,8 +49,8 @@ const checkRoshIsLive = async (): Promise<boolean> => {
 };
 
 const initAndConnectAccounts = (): void => {
-    console.log("[\x1b[36mDETECTED\x1b[0m]: Rosh started streaming...");
-    console.log("[\x1b[36mACTION\x1b[0m]: Connecting accounts...");
+    logger.info("Rosh started streaming...");
+    logger.info("Connecting accounts...");
     for (let i =0; i < accounts.length; i++){
         try{
             const client = new tmi.Client({
@@ -62,33 +64,33 @@ const initAndConnectAccounts = (): void => {
                 channels: accounts[i].channels
             });
 
-            client.connect().catch(error => console.log(`${error} - ${accounts[i].username}`));
+            client.connect().catch(error => logger.error(`${error} - ${accounts[i].username}`));
 
             client.on("connected", (a: string, b: number) => {
-                console.log(`${accounts[i].username}: ready`, a, b);
+                logger.info(`${accounts[i].username}: ready`, a, b);
 
                 clients.push(client);
             })
         }catch(err){
-            console.log(err);
+            logger.error(err);
         }
     }
 };
 
 const destroyAccounts = (): void => {
-    console.log("[\x1b[36mDETECTED\x1b[0m]: Rosh went offline.")
-    console.log("[\x1b[36mACTION\x1b[0m]: Start disconnecting accounts...");
+    logger.info("Rosh went offline.");
+    logger.info("Start disconnecting accounts...");
     for (let i=0; i < clients.length; i++){
         clients[i].disconnect();
 
         clients.splice(i, 1);
     }
 
-    console.log("Accounts have been disconnected");
+    logger.info("Accounts have been disconnected");
 };
 
 const startLoop = (): void => {
-    console.log("[\x1b[36mACTION\x1b[0m]: Starting loop...");
+    logger.info("Starting loop...");
     setInterval(async () => {
         const live = await checkRoshIsLive();
 
---
+++ src/logger/Logger.ts
@@ -0,0 +1,73 @@
+export class Logger{
+
+    private dateColor: string;
+    private processColor: string;
+    private pathColor: string;
+    private infoColor: string;
+    private warningColor: string;
+    private errorColor: string;
+
+    constructor(
+        dateColor = "\x1b[36m%s\x1b[0m",
+        processColor = "\x1b[35m",
+        pathColor = "\x1b[34m",
+        infoColor = "\x1b[32mINFO\x1b[0m",
+        warningColor = "\x1b[33mWARNING\x1b[0m",
+        errorColor = "\x1b[31mERROR\x1b[0m"
+    ){
+        this.dateColor = dateColor;
+        this.processColor = processColor;
+        this.pathColor = pathColor;
+        this.infoColor = infoColor;
+        this.warningColor = warningColor;
+        this.errorColor = errorColor;
+    }
+
+    public getCurrentDateFormat(){
+        let date = new Date();
+        return {color: this.dateColor, date:`${date.getFullYear()}-${date.getMonth() < 10? "0" + (date.getMonth()+1): (date.getMonth()+1)}-${date.getDate() < 10? "0" + date.getDate():date.getDate()} ${date.getHours() < 10?"0" + date.getHours():date.getHours()}:${date.getMinutes() < 10? "0" + date.getMinutes():date.getMinutes()}:${date.getSeconds() < 10? "0" + date.getSeconds(): date.getSeconds()}.${date.getMilliseconds()}`}
+    }
+
+    public getCallerFile(position = 2){
+        if (position >= Error.stackTraceLimit) {
+            throw new TypeError('getCallerFile(position) requires position be less then Error.stackTraceLimit but position was: `' + position + '` and Error.stackTraceLimit was: `' + Error.stackTraceLimit + '`');
+          }
+        
+          const oldPrepareStackTrace = Error.prepareStackTrace;
+          Error.prepareStackTrace = (_, stack)  => stack;
+          const stack = new Error().stack;
+          Error.prepareStackTrace = oldPrepareStackTrace;
+          
+          var errorPath: string | undefined = "";
+        
+          if (stack !== null && typeof stack === 'object') {
+            errorPath = stack[position] ? (stack[position] as any).getFileName() : undefined;
+          }
+      
+          var fileName:string | undefined = "";
+      
+          for(var i=errorPath.length-1; i > 0 ; i--){
+              if (errorPath[i] === "\\")
+                  break;
+      
+              fileName += errorPath[i];
+          }
+      
+          return fileName.split("").reverse().join("");
+    }
+
+    public info(...args: any[]){
+        let date = this.getCurrentDateFormat();
+        console.log(date.color, date.date, this.processColor, process.pid, this.pathColor, this.getCallerFile(), this.infoColor, ...args);
+    }
+
+    public warning(...args: any[]){
+        let date = this.getCurrentDateFormat();
+        console.log(date.color, date.date, this.processColor, process.pid, this.pathColor, this.getCallerFile(), this.warningColor, ...args);
+    }
+
+    public error(...args: any[]){
+        let date = this.getCurrentDateFormat()
+        console.log(date.color, date.date, this.processColor, process.pid, this.pathColor, this.getCallerFile(), this.errorColor, ...args);
+    }
+}
